c5e3db8d153ed10e6d568fa80e25f2da168b7200
formellaalex <formella.alex@gmail.com>
2015-04-11 11:44:27 +0200

    Mysql configuration

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var mysql = require('mysql')
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -55,6 +55,37 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
+
+db_config = {
+  host     : 'eu-cdbr-west-01.cleardb.com',
+  user     : 'bef07653041975',
+  password : '4c51f22a',
+  database : 'heroku_c58b11caa9227cc',
+  multipleStatements: true 
+};
+
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('Reconnect :', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 app.listen(3000);
 
